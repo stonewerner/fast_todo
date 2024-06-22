cdd5fbb6f47932cd58e42e957f5a5caa11bca141
Stone Werner <stonewerner@berkeley.edu>
2024-06-22 17:18:22 -0500

    changed db, main, models for book context

--- models.py
+++ models.py
@@ -2,11 +2,13 @@ from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
 
-class Todo(Base):
-    __tablename__ = "todos"
+class Book(Base):
+    __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
-    complete = Column(Boolean, default=False)
+    author = Column(String)
+    description = Column(String)
+    read = Column(Boolean, default=False)
 
 # would add schemas here?
